0fbf4fb8283fe0d93c66a4e3a735f5cc81bb761b
zabelra <rebecca_zabel@rhoworld.com>
2016-05-02 18:39:39 +0000

    update cv code

--- Anly/BasicModel_CV/cv.R
+++ Anly/BasicModel_CV/cv.R
@@ -1,16 +1,25 @@
-#' Package: Requires version >=0.1.8 of h2oEnsemble 
-pacman::p_load(h2oEnsemble, caret)
+# Package: Requires version >=0.1.8 of h2oEnsemble 
+pacman::p_load(h2oEnsemble)
 
-#' Start an H2O cluster with nthreads = num cores on your machine
+# Start an H2O cluster with nthreads = num cores on your machine
 localH2O <-  h2o.init(nthreads = -1)
 
-#' Import a sample binary outcome train/test set into R
-train0 <- readRDS("~/useR16_ensemble/Data/Derive/train.rds")
-test0  <- readRDS("~/useR16_ensemble/Data/Derive/test.rds")
+# Import a sample binary outcome train/test set into R
+train <- readRDS("~/useR16_ensemble/Data/Derive/train.rds")
+test  <- readRDS("~/useR16_ensemble/Data/Derive/test.rds")
 
-#' model params
+y <- "TARGET"
+x <- setdiff(names(train), y)
 family <- "binomial"
-nCV <- 5
+nfolds <- 3
+
+# For binary classification, response should be a factor
+train[,y] <- as.factor(train[,y])  
+
+# Send data to h2o
+train <- as.h2o(train)
+test  <- as.h2o(test)
+
 
 #' Specify the base learner library & the metalearner
 h2o.glm.1 <- function(..., alpha = 0.00, solver='L_BFGS') h2o.glm.wrapper(..., alpha = alpha, solver = solver)
@@ -19,34 +28,47 @@ h2o.glm.3 <- function(..., alpha = 0.99)                  h2o.glm.wrapper(..., a
 
 learner <- c("h2o.glm.1","h2o.glm.2","h2o.glm.3")
 
-#' Specify a defalt GLM as the metalearner
+# Specify a defalt GLM as the metalearner
 h2o.glm_nn <- function(..., non_negative = TRUE) h2o.glm.wrapper(..., non_negative = non_negative)
 
-metalearner <- "h2o.glm.wrapper" 
+# Train the ensemble using 5-fold CV to generate level-one data
+# More CV folds will take longer to train, but should increase performance
+fit <- h2o.ensemble(x = x, y = y, 
+                    training_frame = train, 
+                    family = family, 
+                    learner = learner, 
+                    metalearner = "h2o.glm_nn",
+                    cvControl = list(V = nfolds , shuffle = TRUE))
+
+h2o.getFrame(model$basefits[[1]]@parameters$training_frame)
+Y <- fit$basefits[[1]]@parameters$y
 
 
 #' cross validation
 
-h2o_cv <- function(data, Y, K=5, B=1, seed=1000){
+h2o_cv <- function(model, K=5, B=1, seed=1000){
  
-   results <- data.frame('repeatnum'=NA, 'foldnum'=NA, 'error'=NA)
+  results <- data.frame('repeatnum'=NA, 'foldnum'=NA, 'error'=NA)
   n <- 1
   
+  d <- h2o.getFrame(model$basefits[[1]]@parameters$training_frame)
+
   set.seed(seed)
-  ind <- caret::createMultiFolds(data[,Y], k = K, times=B)
+  ind <- caret::createMultiFolds(d[,model$y], k = K, times=B)
   
-  data.h2o <- as.h2o(data)
+#  data.h2o <- as.h2o(data)
   
   for (b in 1:B){
     for (k in 1:K) {
       
-      train <- data.h2o[-ind[[b*k]],]
-      valid <- data.h2o[ind[[b*k]],]
+      
+      train <- d[-ind[[b*k]],]
+      valid <- d[ind[[b*k]],]
       
       #' Find X & Y
-      y <- Y
-      x <- setdiff(names(train), y)
-      train[,y] <- as.factor(train[,y])  
+    # y <- Y
+      x <- setdiff(names(train), model$y)
+      train[,model$y] <- as.factor(train[,model$y])  
       
       #' fit the ensemble
       fit <- h2o.ensemble(x = model$x, y = model$y,
@@ -54,7 +76,7 @@ h2o_cv <- function(data, Y, K=5, B=1, seed=1000){
                           family = model$family,
                           learner = model$learner,
                           metalearner = model$metalearner,
-                          cvControl = list(V = model$cvControl$V, shuffle = model$cvControl$suffle))
+                          cvControl = list(V = model$cvControl$V, shuffle = model$cvControl$shuffle))
       
       #' Predict on validation set
       predicted <- as.vector(predict(fit,valid)$pred$predict)
@@ -72,10 +94,6 @@ h2o_cv <- function(data, Y, K=5, B=1, seed=1000){
   return(results)
 }
 
-h2o.getFrame(fit$metafit@parameters$training_frame)
-h2o.getFrame(fit$basefits[[1]]@parameters$training_frame)
-
-fit$metafit@parameters$training_frame
 
 train0_sub <- train0[1:1000,]  ### make dataset small for development
 results  <- h2o_cv(data=train0_sub, Y='TARGET', K=5, B=2, seed=1000)
