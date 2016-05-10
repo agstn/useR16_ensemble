00fa72d29aa9f41be232ab820ebe347d271ea529
Agustin Calatroni <acalatroni@gmail.com>
2016-05-10 18:10:35 +0000

    Update to cv function

--- Anly/BasicModel_CV/cv.R
+++ Anly/BasicModel_CV/cv.R
@@ -1,28 +1,28 @@
 # Package: Requires version >=0.1.8 of h2oEnsemble 
 pacman::p_load(h2oEnsemble)
 
+# Suppress warnings
+options(warn=-1)
+
 # Start an H2O cluster with nthreads = num cores on your machine
 localH2O <-  h2o.init(nthreads = -1)
 
-# Import a sample binary outcome train/test set into R
-train <- readRDS("~/useR16_ensemble/Data/Derive/train.rds")
-test  <- readRDS("~/useR16_ensemble/Data/Derive/test.rds")
+# Bring data into H2O
+train <- h2o.uploadFile(path = "~/useR16_ensemble/Data/Raw/train.csv", destination_frame = "train")
+test <- h2o.uploadFile(path = "~/useR16_ensemble/Data/Raw/test.csv", destination_frame = "test")
 
-y <- "TARGET"
-x <- setdiff(names(train), y)
+# Setup Model
+y      <- "TARGET"
+x      <- setdiff(names(train), y)
 family <- "binomial"
-nfolds <- 3
+nfolds <- 5
 
 # For binary classification, response should be a factor
 train[,y] <- as.factor(train[,y])  
 
-# Send data to h2o
-train <- as.h2o(train)
-test  <- as.h2o(test)
-
 # Subset data
 train <- train[1:1000,]
-test  <- test[1:1000,]
+test  <-  test[1:1000,]
 
 #' Specify the base learner library & the metalearner
 h2o.glm.1 <- function(..., alpha = 0.00, solver='L_BFGS') h2o.glm.wrapper(..., alpha = alpha, solver = solver)
@@ -35,7 +35,6 @@ learner <- c("h2o.glm.1","h2o.glm.2","h2o.glm.3")
 h2o.glm_nn <- function(..., non_negative = TRUE) h2o.glm.wrapper(..., non_negative = non_negative)
 
 # Train the ensemble using 5-fold CV to generate level-one data
-# More CV folds will take longer to train, but should increase performance
 fit <- h2o.ensemble(x = x, y = y,
                     training_frame = train, 
                     family = family, 
@@ -43,79 +42,42 @@ fit <- h2o.ensemble(x = x, y = y,
                     metalearner = "h2o.glm_nn",
                     cvControl = list(V = nfolds , shuffle = TRUE))
 
-# sink("sink-examp.txt")
-# str(fit)
-# sink()
-# unlink("sink-examp.txt")
-
-#' cross validation
-#' 
-model = fit
-K=3 
-B=2 
-seed=1000
-
-h2o_cv <- function(model, K=5, B=1, seed=1000){
+# Create Cross Validation Function w/ Performance OutperLoop Metric
+h2o_cv <- function(model, K = 3, times = 2, seed = 1000){
   
-  results <- data.frame('repeatnum'=NA, 'foldnum'=NA, 'error'=NA,
-                        'MSE'=NA, 'r2'=NA, 'logloss'=NA, 'AUC'=NA, 
-                        'Gini'=NA, 'residual_deviance'=NA, 'null_deviance'=NA, 'AIC'=NA )
-  n <- 1
-  
-  d <- h2o.getFrame(model$basefits[[1]]@parameters$training_frame)
-  d <- d[,-length(d)]
-  
+  model = fit; K = 2; times = 2; seed = 1000;
+  d <- train
   set.seed(seed)
-  ind <- caret::createMultiFolds(as.vector(d[,model$y]), k = K, times=B)
+  ind <- caret::createMultiFolds(as.vector(d[,model$y]), k = K, times = times)
+  out <- vector("list", length(ind))
+  names(out) <- names(ind)
   
-  for (b in 1:B){
-    for (k in 1:K) {
-      
-      train <- d[ind[[b*k]],]
-      valid <- d[-ind[[b*k]],]
-      
-      #' Find X & Y
-      # y <- Y
-      #x <- setdiff(names(train), model$y)
-      #train[,model$y] <- as.factor(train[,model$y])  
-      
-      #' fit the ensemble
-      fit <- h2o.ensemble(x = model$x, y = model$y,
-                          training_frame = train,
-                          family = model$family,
-                          learner = model$learner,
-                          metalearner = model$metalearner,
-                          cvControl = list(V = model$cvControl$V, shuffle = model$cvControl$shuffle))
-
-      predicted <- as.vector(predict(fit,valid)$pred$predict)
-      compare <- data.frame('predicted'=predicted, 'observed'=as.vector(valid[,y]))
-      compare$match <- I(compare$predicted==compare$observed)
-      
-      results[n, 'repeatnum'] <- b
-      results[n, 'foldnum'] <- k
-      results[n, 'error'] <- sum(compare$match)/length(compare$match)
-      
-      #' Predict on validation set
-      perf <- h2o.ensemble_performance(fit, newdata = valid)$ensemble@metrics
-       
-      results[n, 'MSE'] <- perf$MSE                           
-      results[n, 'r2'] <- perf$r2                        
-      results[n, 'logloss'] <- perf$logloss                       
-      results[n, 'AUC'] <- perf$AUC                           
-      results[n, 'Gini'] <- perf$Gini
-      results[n, 'residual_deviance'] <- perf$residual_deviance
-      results[n, 'null_deviance'] <- perf$null_deviance
-      results[n, 'AIC'] <- perf$AIC
-
-      
-      
-      n <- n+1
-    }
+  for (j in 1:length(ind)){
+    print(paste0("Begin outer cross-validation loop: ", j, " of ", K*times))
+    t <- d[ ind[[j]],]
+    v <- d[-ind[[j]],]
+    # fit the ensemble
+    f <- h2o.ensemble(x = model$x, y = model$y,
+                      training_frame = t,
+                      family   = model$family,
+                      learner  = model$learner,
+                      metalearner = model$metalearner,
+                      cvControl = list(V = model$cvControl$V, shuffle = model$cvControl$shuffle))
+    # Predict on validation set
+    out[[j]] <- h2o.ensemble_performance(f, newdata = v, score_base_models = FALSE)$ensemble
   }
-  
-  return(results)
+  return(out)
 }
 
+fit_cv  <- h2o_cv(model = fit, K = 2, times = 2, seed = 1000)
 
-results  <- h2o_cv(model = fit, K=5, B=2, seed=1000)
+names(fit_cv[[1]]@metrics)
+fit_cv[[1]]@metrics$AUC
+AUC  <- sapply(seq(length(fit_cv)), function(l)  fit_cv[[l]]@metrics$AUC)
+AUC
+# out[[1]]@metrics$thresholds_and_metric_scores
+# out[[1]]@metrics$max_criteria_and_metric_scores
+
+# All done, shutdown H2O
+# h2o.shutdown(prompt=FALSE)
 
